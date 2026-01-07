087a9ad41e606c01a2bc0f7bbbcd13369b1b2087
lucaspaese <30931828+lucaspaese@users.noreply.github.com>
2026-01-07 15:32:47 +0000

    test

--- 16h.py
+++ 16h.py
@@ -5,11 +5,7 @@ from PIL import Image
 
 dossier_actuel = os.path.dirname(os.path.abspath(__file__))
 dossier_images = os.path.join(dossier_actuel, 'images')
-<<<<<<< HEAD
 vict = Image.open(os.path.join(dossier_images, 'vict.jpg'))
-=======
-vict = Image.open(os.path.join(dossier_images, 'vict.png'))
->>>>>>> e18e3ab05b8716cdbd361138279f3b27fc296573
 
 now = datetime.now()
 hour = now.hour
@@ -18,11 +14,9 @@ weekday = now.weekday()   # lundi = 0 ... dimanche = 6
 # --- Partie 1 : Vérifier 16h ---
 if hour >= 15:
     st.title("🕓 C'est déjà 16 heures !")
-<<<<<<< HEAD
-    st.image(vict, width=600)
-=======
-    st.image(vict, width=60)
->>>>>>> e18e3ab05b8716cdbd361138279f3b27fc296573
+
+    st.image(vict, width=400)
+
 else:
     st.title("⏳ Ce n'est pas encore 16 heures.")
 
@@ -41,8 +35,6 @@ if weekday == 4:  # 4 = vendredi
 st.write("---")
 
 st.markdown("Savoir s’il est déjà 16 h revêt une importance particulière dans le cadre de la journée de travail. Cette heure symbolise un seuil, celui où les obligations professionnelles commencent à laisser place au temps personnel. Elle permet de prendre conscience du temps écoulé, d’évaluer l’avancement des tâches en cours et d’anticiper la fin de la journée. Être attentif à l’heure facilite également une meilleure organisation. À l’approche de 16 h, il devient possible de prioriser les actions restantes, de clôturer les dossiers importants et de préparer une transition sereine entre le travail et le retour à la maison. Cela contribue à limiter les imprévus de dernière minute et à éviter le stress lié à une mauvaise gestion du temps.Par ailleurs, connaître précisément l’heure est essentiel pour respecter ses engagements personnels. Le départ du travail conditionne souvent les horaires de transport, les responsabilités familiales ou simplement le besoin légitime de repos. Savoir qu’il est déjà 16 h permet donc de planifier son trajet, d’optimiser son temps de déplacement et de préserver un équilibre sain entre vie professionnelle et vie privée. Enfin, cette attention portée à l’heure traduit une reconnaissance de la valeur du temps. Elle rappelle que le travail, aussi important soit-il, s’inscrit dans une journée plus large où le bien-être personnel a toute sa place. Ainsi, savoir s’il est déjà 16 h n’est pas un simple détail, mais un repère essentiel pour mieux vivre sa journée et rentrer chez soi dans de bonnes conditions.")
-<<<<<<< HEAD
-=======
 
->>>>>>> e18e3ab05b8716cdbd361138279f3b27fc296573
+
 
